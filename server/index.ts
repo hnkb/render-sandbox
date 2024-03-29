9e72bef85fa569618127ce3875ea7f8881da58a7
Hani Kaabi <hani@miro.com>
2024-03-29 11:12:18 +0100

    Serve static files to HTTP

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,19 @@
-console.log(`Hello World!`);
+import { join } from 'path';
+
+const server = Bun.serve({
+	port: 80,
+	async fetch(req) {
+		const path = new URL(req.url).pathname;
+
+		if (path === '/')
+			return new Response(Bun.file(join(import.meta.dir, '../client/public/index.html')));
+
+		const file = Bun.file(join(import.meta.dir, "../client/public", path));
+		if (file.size > 0)
+			return new Response(file);
+
+		return new Response('404 Not Found', { status: 404, statusText: 'Not Found' });
+	}
+})
+
+console.log(`Listening on ${server.url}`);
